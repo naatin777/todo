9d0409c8a381380b0c11147975d60ed3e52f6184
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2022-12-24 15:30:16 +0900

    refactor: rename from router to router_provider

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:todo/ui/providers/router.dart';
+import 'package:todo/ui/providers/router_provider.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
