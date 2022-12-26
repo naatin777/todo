857916e471634e42e499a3154b0621581054fc2d
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2022-12-26 13:54:01 +0900

    feat: change settings app bar animation

--- lib/ui/pages/home/screens/settings/settings_screen.dart
+++ lib/ui/pages/home/screens/settings/settings_screen.dart
@@ -8,68 +8,63 @@ class SettingsScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeProvider);
-    return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return [
-          const SliverAppBar(
-            title: Text("Settings"),
-            pinned: true,
-          ),
-        ];
-      },
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: ListTile(
-              title: const Text("Theme"),
-              subtitle: Text(themeMode.title),
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text("Choose theme"),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: ThemeMode.values
-                            .map(
-                              (e) => ListTile(
-                                leading: Radio<ThemeMode>(
-                                  value: e,
-                                  groupValue: themeMode,
-                                  onChanged: (value) async {
-                                    Navigator.of(context).pop();
-                                    await ref
-                                        .watch(themeProvider.notifier)
-                                        .changeThemeMode(e);
-                                  },
-                                ),
-                                title: Text(e.title),
-                                onTap: () async {
+    return CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          title: Text("Settings"),
+          floating: true,
+          snap: true,
+        ),
+        SliverToBoxAdapter(
+          child: ListTile(
+            title: const Text("Theme"),
+            subtitle: Text(themeMode.title),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("Choose theme"),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: ThemeMode.values
+                          .map(
+                            (e) => ListTile(
+                              leading: Radio<ThemeMode>(
+                                value: e,
+                                groupValue: themeMode,
+                                onChanged: (value) async {
                                   Navigator.of(context).pop();
                                   await ref
                                       .watch(themeProvider.notifier)
                                       .changeThemeMode(e);
                                 },
-                                horizontalTitleGap: 0.0,
                               ),
-                            )
-                            .toList(),
-                      ),
-                      contentPadding: EdgeInsets.only(
-                        left: 0.0,
-                        top: Theme.of(context).useMaterial3 ? 16.0 : 20.0,
-                        right: 0.0,
-                        bottom: 24.0,
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          )
-        ],
-      ),
+                              title: Text(e.title),
+                              onTap: () async {
+                                Navigator.of(context).pop();
+                                await ref
+                                    .watch(themeProvider.notifier)
+                                    .changeThemeMode(e);
+                              },
+                              horizontalTitleGap: 0.0,
+                            ),
+                          )
+                          .toList(),
+                    ),
+                    contentPadding: EdgeInsets.only(
+                      left: 0.0,
+                      top: Theme.of(context).useMaterial3 ? 16.0 : 20.0,
+                      right: 0.0,
+                      bottom: 24.0,
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
