77a3fb6cf0c6fe86d8b3666d99c9ae3c38ef44f2
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2023-04-10 23:08:58 +0900

    feat: change navigation drawer ui

--- lib/ui/pages/home/screens/task/project_drawer.dart
+++ lib/ui/pages/home/screens/task/project_drawer.dart
@@ -9,6 +9,76 @@ class ProjectDrawer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final projects = ref.watch(projectDrawerStreamProvider);
+    final projectDrawer = ref.watch(projectDrawerProvider);
+    return projects.when(
+      data: (data) => NavigationDrawer(
+        selectedIndex: () {
+          if (projectDrawer.id == inbox.id) {
+            return 0;
+          } else {
+            return data
+                    .asMap()
+                    .entries
+                    .firstWhere(
+                        (element) => element.value.id == projectDrawer.id)
+                    .key +
+                1;
+          }
+        }(),
+        onDestinationSelected: (int index) {
+          if (index == 1 + data.length) {
+            showDialog(
+              context: context,
+              builder: (context) => const AddNewProjectDialog(),
+            );
+          } else {
+            ref.read(projectDrawerProvider.notifier).changeProject(() {
+              for (final e in data.asMap().entries) {
+                if (e.key + 1 == index) {
+                  return e.value;
+                }
+              }
+              return inbox;
+            }());
+            Navigator.of(context).pop();
+          }
+        },
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
+            child: Text(
+              "Lists",
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+          const Divider(),
+          NavigationDrawerDestination(
+            icon: const Icon(Icons.inbox),
+            label: Text(inbox.title),
+            selectedIcon: const Icon(Icons.inbox_outlined),
+          ),
+          const Divider(),
+          for (final e in data)
+            NavigationDrawerDestination(
+              icon: const Icon(Icons.list),
+              label: Text(e.title),
+              selectedIcon: const Icon(Icons.list_outlined),
+            ),
+          const Divider(),
+          const NavigationDrawerDestination(
+            icon: Icon(Icons.add),
+            label: Text("Add new project"),
+          ),
+        ],
+      ),
+      error: (error, stackTrace) => ListTile(
+        title: Text(error.toString()),
+        subtitle: Text(stackTrace.toString()),
+      ),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
     return Drawer(
       child: SafeArea(
         child: ListView(
